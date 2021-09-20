ce6a2c22f40fe3f9a7f555fbdf298f3932a3447b
Sohrab Nazari <sohrab.nzr98@gmail.com>
2021-09-20 22:51:25 +0430

    Reactjs added to project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [reactRefresh()]
 })
